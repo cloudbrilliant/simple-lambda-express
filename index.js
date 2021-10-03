ae12a7f1185b9e8a271f53951e39c1fa87f6d6bc
Michael Gill <mike@cloudgalacticos.co.uk>
2021-10-03 16:18:45 +0100

    Simple Lambda Express

---
+++ index.js
@@ -0,0 +1,22 @@
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res, next) => {
+    return res.status(200).json({
+        message: 'Hello from root!',
+    });
+});
+
+app.get('/hello', async (req, res, next) => {
+    const result = res.status(200).json({ message: 'Hello Async' });
+    return result;
+});
+
+app.use((req, res, next) => {
+    return res.status(404).json({
+        error: 'Not Found',
+    });
+});
+
+module.exports.handler = serverless(app);
